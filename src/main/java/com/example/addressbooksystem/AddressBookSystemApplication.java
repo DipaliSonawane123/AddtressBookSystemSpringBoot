f4f0d2c3530d788546a16db68cfae8a94cdc4da3
Dipali <dipssonawane123@gmail.com>
2022-09-05 10:31:47 +0530

    Added:AddressBook syatem Project is added

---
+++ src/main/java/com/example/addressbooksystem/AddressBookSystemApplication.java
@@ -0,0 +1,19 @@
+package com.example.addressbooksystem;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class AddressBookSystemApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AddressBookSystemApplication.class, args);
+        System.out.println("Hello!!");
+        log.info("Application Loaded Successfully.");
+    }
+    }
+
+
